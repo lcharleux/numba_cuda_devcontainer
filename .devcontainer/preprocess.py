dee37c631cd293166886e526712831bc4ed3235c
lcharleux <ludovic.charleux@univ-smb.fr>
2023-01-18 09:21:35 +0000

    Improved container behavior

--- .devcontainer/preprocess.py
+++ .devcontainer/preprocess.py
@@ -5,16 +5,19 @@ import json
 import shutil
 import hashlib
 
+# LOCAL CONFIG
+if not os.path.isfile("my_config.json"):
+    shutil.copy("my_config_default.json", "my_config.json")
+my_config = json.load(open("my_config.json"))
+my_config_default = json.load(open("my_config_default.json"))
+for k, v in my_config_default.items():
+    if k not in my_config.keys():
+        my_config[k] = v
+
 # UNIQUE HASH VALUE
 current_dir = os.getcwd()
 hash_value = hashlib.sha256(str.encode(current_dir)).hexdigest()
 user_name = os.environ.get("USER")
-
-
-# MY VOLUMES
-if not os.path.isfile("my_volumes.json"):
-    shutil.copy("my_volumes_template.json", "my_volumes.json")
-my_volumes = json.load(open("my_volumes.json"))
 
 data = {
     "USER_NAME": user_name,
@@ -22,18 +25,17 @@ data = {
     "GID": str(pwd.getpwnam(os.environ.get("USER")).pw_gid),
     "TAG": "0.0",
     "CONTAINER_NAME": f"{user_name}_{hash_value}",
+    "GPU_COUNT": my_config["gpu_count"],
 }
 
 # TEMPLATE
 dct = Template(open("docker-compose_template.yml").read().strip()).substitute(data)
 
-
+my_volumes = my_config["volumes"]
 if len(my_volumes) != 0:
     dct += "\n" + 4 * " " + "volumes:" + "\n" + 6 * " "
 for volume in my_volumes:
-    dct += "- {0}:{1}".format(volume["host"], volume["target"])
-    if "read_only" in volume.keys() and volume["read_only"]:
-        dct += ":ro"
+    dct += "- {0}".format(volume)
     dct += "\n" + 6 * " "
 
 dct = dct.strip()
@@ -41,5 +43,8 @@ dct = dct.strip()
 open("docker-compose.yml", "w").write(dct)
 
 # ENV FILE CREATION
-
 open("../.env", "w").write(f"COMPOSE_PROJECT_NAME={user_name}_{hash_value}")
+
+# CUSTOM CONDA ENV CREATION
+if not os.path.isfile("my_environment.yml"):
+    shutil.copy("my_environment_example.yml", "my_environment.yml")
